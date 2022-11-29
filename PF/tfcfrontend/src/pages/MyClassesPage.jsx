12d4af4b34cf398835e84bc319639267ba9c698d
valyippee <valerieyipfh@gmail.com>
2022-11-29 18:19:40 -0500

    finalized look of calendar view

--- PF/tfcfrontend/src/pages/MyClassesPage.jsx
+++ PF/tfcfrontend/src/pages/MyClassesPage.jsx
@@ -7,19 +7,25 @@ import './AccountPage.css';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import Popover from 'react-bootstrap/Popover';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Event({ event }) {
-  let popoverClickRootClose = (
-    <Popover id="event-popover" style={{ height: "8em", width: "18em"}}>
-      <strong>{event.title}</strong>
+  let popover = (
+    <Popover id="event-popover">
+      <strong id="event-title">{event.title}</strong>
       <br/>
+      <span id='event-description'>
+        {event.description}<br/><br/>
+        <strong>Location: </strong>{event.location}<br/>
+        <strong>Coach: </strong> {event.coach}<br/>
+        <strong>Time: </strong> {event.start.toLocaleTimeString()} - {event.end.toLocaleTimeString()}
+      </span>
       {event.end > new Date() && 
-        <div>
-          <button id="drop-instance">Drop this instance</button>
-          <button id="drop-class">Drop this recurring class</button>
+        <div id='drop-buttons'>
+          <button className="btn btn-lg btn-primary" id="drop-instance">Drop this instance</button>
+          <button className="btn btn-lg btn-primary" id="drop-class">Drop this recurring class</button>
         </div>
       }
-      
     </Popover>
   );
 
@@ -27,7 +33,7 @@ function Event({ event }) {
   return (
     <div>
       <div>
-        <OverlayTrigger id="class-info" trigger="click" rootClose container={this} placement="top" overlay={popoverClickRootClose}>
+        <OverlayTrigger id="class-info" trigger="click" rootClose container={this} placement="auto-end" overlay={popover}>
           <div>{event.title}</div>
         </OverlayTrigger>
       </div>
@@ -46,16 +52,25 @@ function MyClassesPage() {
         title: 'Phone Interview',
         start: new Date(2022, 10, 27, 17, 0, 0),
         end: new Date(2022, 10, 27, 18, 30, 0),
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        location: "Studio 1",
+        coach: "Mary"
       },
       {
         title: 'Cooking Class',
         start: new Date(2022, 10, 27, 17, 30, 0),
         end: new Date(2022, 10, 27, 19, 0, 0),
+        description: "cooking your favourite dish",
+        location: "Studio 2",
+        coach: "Mary"
       },
       {
         title: 'Go to the gym',
-        start: new Date(2022, 10, 28, 18, 30, 0),
-        end: new Date(2022, 10, 28, 20, 0, 0),
+        start: new Date(2022, 10, 30, 18, 30, 0),
+        end: new Date(2022, 10, 30, 20, 0, 0),
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        location: "Studio 3",
+        coach: "Mary"
       },
     ]
   );
@@ -63,7 +78,7 @@ function MyClassesPage() {
   const eventStyleGetter = (event) => { 
     console.log(event)
     var style = { 
-      backgroundColor: event.end > new Date() ? "#656475" : "#7f7d96",
+      backgroundColor: event.end > new Date() ? "#403E56" : "#7f7d96",
     }; 
     return { style: style }; 
   }; 
