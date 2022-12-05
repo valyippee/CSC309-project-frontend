cf43a1bb921fc7b0360130821d5bc716fa1ea6c8
1moonala <junseomoon9@gmail.com>
2022-12-05 18:01:09 -0500

    Added subscription component to SubscriptionsPage

--- PF/tfcfrontend/src/pages/SubscriptionsPage.jsx
+++ PF/tfcfrontend/src/pages/SubscriptionsPage.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import "./SubscriptionsPage.css"
+import React, { useEffect, useState } from "react";
+import "./SubscriptionsPage.css";
 import Title from "../components/Title";
+import { getSubscriptionPlans } from "../api/requests";
+import Subscription from "../components/subscriptions/Subscription";
 
 const SubscriptionsPage = () => {
+  const [subscriptions, setSubscriptions] = useState([]);
+
+  useEffect(() => {
+    getSubscriptionPlans(setSubscriptions);
+  }, []);
+
   return (
     <>
-        <Title title={"Subscriptions"} />
-        <div className="subscriptions-page-body-container">
-            
-        </div>
+      <Title title={"Subscriptions"} />
+      <div className="subscriptions-page-body-container">
+        {subscriptions.map((subscription) => (
+          <Subscription subscription={subscription} />
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default SubscriptionsPage
+export default SubscriptionsPage;
