bffdd13df5dcfcc5acc0b2e3409a9146621064d4
valyippee <valerieyipfh@gmail.com>
2022-12-08 19:27:51 -0500

    move all startup and run scripts to root

--- PF/startup.sh
+++ PF/startup.sh
@@ -14,10 +14,22 @@ python3 -m virtualenv -v venv
 source venv/bin/activate
 
 # backend startup
-./tfcbackend/startup.sh
+cd tfcbackend
+
+# Install Django dependencies
+pip3 install -r requirements.txt
+
+# Run migrations
+python3 manage.py migrate
+
+# Add cron job to check payments due today
+python3 manage.py crontab add
+
+# Populate database with initial data
+python3 manage.py populate_db
 
 # frontend startup
-cd tfcfrontend
+cd ../tfcfrontend
 npm install
 
 cd ..
